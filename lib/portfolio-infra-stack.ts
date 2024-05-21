f9674dd023667825b650d99c55ab0fec7bdb1ea9
pradumkr <pradumkumar123@gmail.com>
2024-05-22 04:12:08 +0530

    Attaching bucket policy to make it publically accessible and S3 as website hosting configuration

--- lib/portfolio-infra-stack.ts
+++ lib/portfolio-infra-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PortfolioInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define an S3 bucket
+    const bucket = new s3.Bucket(this, 'Portfolio', {
+      websiteIndexDocument: 'index.html', // The index document
+      publicReadAccess: false, // Make the contents publicly readable      
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+      autoDeleteObjects: true, // NOT recommended for production code
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the bucket policy
+    const bucketPolicy = new s3.BucketPolicy(this, 'PortfolioPolicy', {
+      bucket,
+    });
+
+    // Add the policy statements
+    bucketPolicy.document.addStatements(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [`${bucket.bucketArn}/*`],
+        principals: [new iam.AnyPrincipal()],
+      })
+    );
   }
 }
